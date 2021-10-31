76ddbce7b65966c84f7430d8e8cbeb51fcda31a1
Justin <justinwilkin@live.ca>
2021-10-31 02:46:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const MathUtil = {
 };
 const ServerData = {
     VERSION: "1.0.0",
-    GAME_VERSION: "0.0.5",
+    GAME_VERSION: "0.0.6",
 };
 const LobbyState = {
     INTERMISSION: "intermission",
