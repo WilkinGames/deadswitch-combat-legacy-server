78fd21edc691a508fa5038142ae992f2cffd502a
Justin <justinwilkin@live.ca>
2021-11-13 23:41:55 -0500

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -10000,7 +10000,7 @@ class GameInstance
             var veh = this.getObjectById(curPawn.data.controllableId);
             if (veh)
             {
-                veh.velocity[1] += 10000;
+                veh.applyImpulse([0, -10000]);
             }
         }
     }
